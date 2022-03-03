1ef140e1789ff4319db509581f77187e2121e2b4
Prasad Jadhav <88767331+DolorPJ@users.noreply.github.com>
2022-03-03 18:22:41 +0530

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <div className="error_container">
+        <h1 className="error404">404</h1>
+        <h1 className="error_oops">Ooops!</h1>
+        <p className="error_message">Page not found</p>
+    </div>
+  )
+}
+
+export default NotFound
